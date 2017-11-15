7918e3b18dc3d1963840ef23a1f0d49e954ba912
m4d32k1llu <pedro.miguel.69@hotmail.com>
2017-11-15 17:23:33 +0000

    Test

---
+++ database.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+hostname = 'db.ist.utl.pt'
+username = 'ist178876'
+password = 'epiphone'
+database = username
+
+# Simple routine to run a query on a database and print the results:
+def doQuery( conn ) :
+    cur = conn.cursor()
+
+    cur.execute( "SELECT nome FROM categoria" )
+
+    for name in cur.fetchall() :
+        print name
+
+
+print "Using psycopg2…"
+import psycopg2
+myConnection = psycopg2.connect( host=hostname, user=username, password=password, dbname=database )
+doQuery( myConnection )
+myConnection.close()
