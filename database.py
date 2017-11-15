7e21f3d9b8ab3b2503e6b80ca95e492e104e17b2
m4d32k1llu <pedro.miguel.69@hotmail.com>
2017-11-15 19:45:50 +0000

    DB

--- database.py
+++ database.py
@@ -17,18 +17,17 @@ def doQuery( conn ) :
 def Login(conn, username, password) :
     cur = conn.cursor()
 
-    cur.execute( "SELECT * FROM users WHERE username = " + username + " AND password = " + password )
+    cur.execute( "SELECT * FROM users WHERE username = '" + username + "'")
 
     for name, pas in cur.fetchall() :
         if(username == name and pas == password):
-			print "Logged in"
-		else
-			print "Incorrect user or pass"
+	    return "Logged in"
+    return "Incorrect username or password"
 	
 print "Using psycopg2"
 import psycopg2
 myConnection = psycopg2.connect( host=hostname, user=username, password=password, dbname=database )
 user = raw_input('username:')
 pas = raw_input('password:')
-Login(user, pas)
+print Login(myConnection, user, pas)
 myConnection.close()
