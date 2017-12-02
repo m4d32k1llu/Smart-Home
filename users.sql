f48c81cfafaecd334681689f28ec26d0483d24b8
m4d32k1llu <pedro.miguel.69@hotmail.com>
2017-12-02 09:00:57 +0000

    .sql

--- users.sql
+++ users.sql
@@ -2,9 +2,8 @@ drop table users cascade;
 
 create table users(
 	username	varchar(80) not null unique,
-	password	varchar(80) not null
+	password	varchar(60) not null
 );
 
-insert into users values ('super', '1ns3cur3');
-insert into users values ('user1', '1ns3cur3');
+insert into users values ('super', '$2b$12$hyx1Z83DP9vyaOYvdbz3GO7tPX5BtQ/qA9aoOThrrjYO9JkbZ0MFC');
 
