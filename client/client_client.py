8acc161fa49d3b49b5ce9d7430f1cc07b596065b
m4d32k1llu <pedro.miguel.69@hotmail.com>
2017-12-02 12:33:53 +0000

    Client Login

--- client/client_client.py
+++ client/client_client.py
@@ -15,14 +15,49 @@ ssl_sock = ssl.wrap_socket(sock,
 
 print >>sys.stderr, '[C] connecting to: %s; port: %s' % client_addr
 ssl_sock.connect(client_addr)
-print "Please Log in..."
-user = raw_input('username:')
-pas = getpass.getpass('password:');
+logged = False
 r = ssl_sock.recv(1024)
 print "[C] received:", r
-s = "login "+ user + " " + pas
-print "[C] sending:", s
-ssl_sock.sendall(s) 
+if r == "Please Log In":
+    user = raw_input('username:')
+    pas = getpass.getpass('password:')
+    s = "login "+ user + " " + pas
+    ssl_sock.sendall(s)
+else:
+    s = "Something went wrong... Exiting"
+    print s
+    ssl_sock.sendall(s)
+    ssl_cock.close()
+    exit()
+r = ssl_sock.recv(1024)
+print "[C] received:", r
+if r == "Login Failed":
+    s = "Login Failed. Exiting..."
+    print "[C] sending:", s
+    ssl_sock.sendall(s)
+    ssl_sock.close()
+    exit()
+elif r == "Login ERROR":
+    s = "Login format was wrong"
+    print "[C] sending:", s
+    ssl_sock.sendall(s)
+    ssl_sock.close()
+    exit()
+elif r == "Login True":
+    logged = True
+    s = "Logged In"
+    ssl_sock.sendall(s)
+else:
+    s = "Something went wrong. Exiting"
+    print "[C] sending:", s
+    ssl_sock.sendall(s)
+    ssl_sock.close()
+    exit()
+if not logged:
+    print "Max attempts reached..."
+    ssl_sock.close()
+    exit()
+ 
 try:
   while True:
     r = ssl_sock.recv(1024)
