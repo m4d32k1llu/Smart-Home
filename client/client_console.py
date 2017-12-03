da9f9fdd60075bd5df0a90f96476f1e022c2be41
m4d32k1llu <pedro.miguel.69@hotmail.com>
2017-12-03 01:48:12 +0000

    little changes

--- client/client_console.py
+++ client/client_console.py
@@ -15,7 +15,6 @@ ssl_sock = ssl.wrap_socket(sock,
 
 print >>sys.stderr, '[C] connecting to: %s; port: %s' % client_addr
 ssl_sock.connect(client_addr)
-logged = False
 r = ssl_sock.recv(1024)
 print "[C] received:", r
 if r == "Please Log In":
@@ -33,28 +32,20 @@ r = ssl_sock.recv(1024)
 print "[C] received:", r
 if r == "Login Failed":
     s = "Login Failed. Exiting..."
-    print "[C] sending:", s
-    ssl_sock.sendall(s)
+    print s
     ssl_sock.close()
     exit()
 elif r == "Login ERROR":
     s = "Login format was wrong"
-    print "[C] sending:", s
-    ssl_sock.sendall(s)
+    print s
     ssl_sock.close()
     exit()
 elif r == "Login True":
-    logged = True
     s = "Logged In"
     ssl_sock.sendall(s)
 else:
     s = "Something went wrong. Exiting"
-    print "[C] sending:", s
-    ssl_sock.sendall(s)
-    ssl_sock.close()
-    exit()
-if not logged:
-    print "Max attempts reached..."
+    print s
     ssl_sock.close()
     exit()
  
